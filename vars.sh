bd06e49e1d02d4cd787c7e6c758245031471316d
Luis Silva <luis.felipe.silva@gmail.com>
2024-04-11 16:08:01 +0100

    adds jdk17

--- vars.sh
+++ vars.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://github.com/bell-sw/Liberica/releases/download/22%2B37/bellsoft-jre22+37-linux-amd64-full.tar.gz
+#export JRE_URL=https://github.com/bell-sw/Liberica/releases/download/22%2B37/bellsoft-jre22+37-linux-amd64-full.tar.gz
+export JRE_URL=https://github.com/bell-sw/Liberica/releases/download/17.0.9%2B11/bellsoft-jre17.0.9+11-linux-amd64-full.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
