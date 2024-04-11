50d6c5564e1ce07af5f0a2a419d06ccba0d2ea39
Luis Silva <luis.felipe.silva@gmail.com>
2024-04-11 14:38:05 +0100

    Updated java version to liberica 22

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+export JRE_URL=https://github.com/bell-sw/Liberica/releases/download/22%2B37/bellsoft-jre22+37-linux-amd64-full.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
